b54829d5ee4578e87e7a38c756f7ab6c18165c6c
Adeba Shahnaz <adeba4343@gmail.com>
2026-02-24 19:47:36 +0530

    Update activity.js

--- activities/VideoViewer.activity/js/activity.js
+++ activities/VideoViewer.activity/js/activity.js
@@ -17,6 +17,8 @@ define(["sugar-web/activity/activity","sugar-web/env","filterpalette","tutorial"
 
 		// Create palette
 		var filterButton = document.getElementById("filter-button");
+		// Disable filter initially
+		filterButton.style.display = "none";
 		filterpalette = new filterpalette.FilterPalette(filterButton, undefined);
 		filterpalette.addEventListener('filter', function() {
 			app.setFilter({category: filterpalette.getFilter()});
