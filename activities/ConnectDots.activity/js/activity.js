ec5d8db62d6319d5eb17d36125aeb97b3d6b40ce
Adeba Shahnaz <adeba4343@gmail.com>
2026-02-25 21:29:04 +0530

    Update activity.js

--- activities/ConnectDots.activity/js/activity.js
+++ activities/ConnectDots.activity/js/activity.js
@@ -1,1 +1,42 @@
+define([], function () {
 
+    const canvas = document.getElementById("gameCanvas");
+    const ctx = canvas.getContext("2d");
+
+    function resize() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+
+    window.addEventListener("resize", resize);
+    resize();
+
+    let dots = [];
+
+    function createDots() {
+        dots = [];
+        for (let i = 0; i < 20; i++) {
+            dots.push({
+                x: Math.random() * canvas.width,
+                y: Math.random() * canvas.height
+            });
+        }
+    }
+
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = "black";
+        dots.forEach(dot => {
+            ctx.beginPath();
+            ctx.arc(dot.x, dot.y, 6, 0, Math.PI * 2);
+            ctx.fill();
+        });
+
+        requestAnimationFrame(draw);
+    }
+
+    createDots();
+    draw();
+
+});
